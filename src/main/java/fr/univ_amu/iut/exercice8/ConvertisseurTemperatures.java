ce57b4feb3fca20ac4117c975fa0cfbb5ea57e77
mjgoat07 <matteo.josuan@etu.univ-amu.fr>
2026-05-11 09:49:53 +0000

    feat: exercice8 - convertisseur temperature capstone

--- src/main/java/fr/univ_amu/iut/exercice8/ConvertisseurTemperatures.java
+++ src/main/java/fr/univ_amu/iut/exercice8/ConvertisseurTemperatures.java
@@ -1,6 +1,15 @@
 package fr.univ_amu.iut.exercice8;
 
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.converter.NumberStringConverter;
 
@@ -28,27 +37,87 @@ public class ConvertisseurTemperatures extends Application {
     // TODO exercice 8 : construire le convertisseur de températures.
     //
     // 1. Créer le panneau Celsius (VBox) :
-    //    - Label "°C" (style bold, 16px)
-    //    - Slider vertical [0, 100], valeur initiale 0, id "slider-celsius"
-    //    - TextField, id "tf-celsius", maxWidth 50
+    // - Label "°C" (style bold, 16px)
+    // - Slider vertical [0, 100], valeur initiale 0, id "slider-celsius"
+    // - TextField, id "tf-celsius", maxWidth 50
     //
     // 2. Créer le panneau Fahrenheit (VBox) :
-    //    - Label "°F" (style bold, 16px)
-    //    - Slider vertical [0, 212], valeur initiale 32, id "slider-fahrenheit"
-    //    - TextField, id "tf-fahrenheit", maxWidth 50
+    // - Label "°F" (style bold, 16px)
+    // - Slider vertical [0, 212], valeur initiale 32, id "slider-fahrenheit"
+    // - TextField, id "tf-fahrenheit", maxWidth 50
     //
     // 3. Ajouter un ChangeListener sur le slider Celsius :
-    //    fahrenheit = celsius * 9/5 + 32
-    //    (utiliser un flag "updating" pour éviter les boucles infinies)
+    // fahrenheit = celsius * 9/5 + 32
+    // (utiliser un flag "updating" pour éviter les boucles infinies)
     //
     // 4. Ajouter un ChangeListener sur le slider Fahrenheit :
-    //    celsius = (fahrenheit - 32) * 5/9
+    // celsius = (fahrenheit - 32) * 5/9
     //
     // 5. Lier chaque TextField à son slider via
-    //    Bindings.bindBidirectional(tf.textProperty(), slider.valueProperty(),
-    //        new NumberStringConverter())
+    // Bindings.bindBidirectional(tf.textProperty(), slider.valueProperty(),
+    // new NumberStringConverter())
     //
     // 6. Composer les panneaux dans un HBox, créer la Scene, afficher.
+    Label labelCelsius = new Label("°C");
+    labelCelsius.setStyle("-fx-font-weight: bold; -fx-font-size: 16px;");
+
+    Slider sliderCelsius = new Slider(0, 100, 0);
+    sliderCelsius.setOrientation(Orientation.VERTICAL);
+    sliderCelsius.setId("slider-celsius");
+
+    TextField tfCelsius = new TextField();
+    tfCelsius.setId("tf-celsius");
+    tfCelsius.setMaxWidth(50);
+
+    Label labelFahrenheit = new Label("°F");
+    labelFahrenheit.setStyle("-fx-font-weight: bold; -fx-font-size: 16px;");
+
+    Slider sliderFahrenheit = new Slider(0, 212, 32);
+    sliderFahrenheit.setOrientation(Orientation.VERTICAL);
+    sliderFahrenheit.setId("slider-fahrenheit");
+
+    TextField tfFahrenheit = new TextField();
+    tfFahrenheit.setId("tf-fahrenheit");
+    tfFahrenheit.setMaxWidth(50);
+
+    sliderCelsius
+        .valueProperty()
+        .addListener(
+            (obs, old, newVal) -> {
+              if (!updating) {
+                updating = true;
+                sliderFahrenheit.setValue(newVal.doubleValue() * 9.0 / 5.0 + 32);
+                updating = false;
+              }
+            });
+    sliderFahrenheit
+        .valueProperty()
+        .addListener(
+            (obs, old, newVal) -> {
+              if (!updating) {
+                updating = true;
+                sliderCelsius.setValue((newVal.doubleValue() - 32) * 5.0 / 9.0);
+                updating = false;
+              }
+            });
+
+    Bindings.bindBidirectional(
+        tfCelsius.textProperty(), sliderCelsius.valueProperty(), new NumberStringConverter());
+    Bindings.bindBidirectional(
+        tfFahrenheit.textProperty(), sliderFahrenheit.valueProperty(), new NumberStringConverter());
+
+    VBox celsiusPane = new VBox(10, labelCelsius, sliderCelsius, tfCelsius);
+    celsiusPane.setAlignment(Pos.CENTER);
+
+    VBox fahrenheitPane = new VBox(10, labelFahrenheit, sliderFahrenheit, tfFahrenheit);
+    fahrenheitPane.setAlignment(Pos.CENTER);
+
+    HBox root = new HBox(20, celsiusPane, fahrenheitPane);
+    root.setAlignment(Pos.CENTER);
+
+    Scene scene = new Scene(root, 300, 300);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
